03ffb094142257300db8d65e891a465bb39bedbb
praveenpiske <piskepraveen@gmail.com>
2023-12-30 15:37:52 +0530

    navbar created

---
+++ src/shared/Navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css';
+
+export default function Navbar() {
+  return (
+    <div>
+      <div class="topnavbar">
+        <a class="active"><i class="fa fa-home"></i></a>
+        <a >Watchlist</a>
+        <a >Search</a>
+        <a >Portfolio</a>
+        <a class="split ">Profile</a>
+
+
+      
+    </div>
+    </div>
+  )
+}
