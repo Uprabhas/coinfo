0745801311e7b43170faabedae7dc1da96fd456a
praveenpiske <piskepraveen@gmail.com>
2023-12-30 16:21:22 +0530

    navbar ui changes

--- src/shared/Navbar/navbar.js
+++ src/shared/Navbar/navbar.js
@@ -5,12 +5,10 @@ export default function Navbar() {
   return (
     <div>
       <div class="topnavbar">
-        <a class="active"><i class="fa fa-home"></i></a>
-        <a >Watchlist</a>
-        <a >Search</a>
-        <a >Portfolio</a>
-        <a class="split ">Profile</a>
-
+        <a class="active"><i class="fa fa-home"> Home</i></a>
+        <a class="split"><i class="fa fa-heart"></i> Watchlist</a>
+        <a class="split "><i class ="fa fa-user"></i> Profile</a>
+        <input type='text' placeholder='Search... Madcoin'></input>
 
       
     </div>
