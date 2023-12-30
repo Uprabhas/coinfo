be8c63f2eec1ebd251ce0989d6a44790af75dbfa
prabhas utla <prabashutla@111gmail.com>
2023-12-30 16:22:56 +0530

    home was created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './shared/Navbar/navbar';
 import Footer from './shared/Footer/footer';
+import Home from './pages/Home/Home';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
