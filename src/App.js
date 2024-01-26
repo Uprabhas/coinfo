f49199bc87cb5f35cfcf1626e5dbd84c848e7180
prabhas utla <prabashutla@111gmail.com>
2024-01-26 12:46:37 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <link
-          rel="stylesheet"q
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
         <Navbar />
