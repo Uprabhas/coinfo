749747774dd13d65bd27446c507a72c3db3bacc6
prabhas utla <prabashutla@111gmail.com>
2024-01-31 23:07:40 +0530

    minor changes

--- src/pages/Coinmodel/coin-detail/Coindetail.js
+++ src/pages/Coinmodel/coin-detail/Coindetail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { api_options, base_URL } from '../../../services/Apiservice';
 
 export default function Coindetail() {
 
-    
+  const [coindatail, setcoindatail] = useState([])
+
+  const{id}=useParams()
+  const getdata=async()=>{
+    const res =await fetch(base_URL+'/coin/'+id,{
+      options:api_options
+    })
+    const data = await res.json()
+    setcoindatail(data)
+    console.log(data);
+
+  }
+
+  useEffect(() => {
+    getdata()
+  
+  },[] )
+  
+
   return (
-    <div>Coindetail</div>
+    <div>
+        <div className='container-fluid'>
+            <div className='header'>
+                <div className='title'>
+                    Bitcoin{id}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
